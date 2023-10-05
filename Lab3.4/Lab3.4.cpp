a7b144b8fce0ed0c2893dfb06ca6b443a9942ad8
unknown <katepob160@gmail.com>
2023-10-05 22:21:35 +0300

    lab 3.4 commit

---
+++ Lab3.4/Lab3.4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double x;
+
+	double y;
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+
+	if ((y >= ((x - 2) * (x - 2) - 3) && (y <= x) && (y >= 0)) ||
+		(y >= ((x - 2) * (x - 2) - 3) && (y <= -x)))
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+}
